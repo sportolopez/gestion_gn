e103268d8e78c5f7741c43a344366762abd8c133
Sporto <sporto@certant.com>
2022-01-18 11:02:03 -0300

    se eliminan class

--- target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO lista (nombre,color) VALUES
-	 ('AMBA','FFD5A2'),
-	 ('GENERAL','A2B2FF'),
-	 ('INTERIOR','A2FFA8'),
-	 ('VIP','F8FA5F');
--- target/classes/iniciar.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-start javaw -jar gestion_gn.jar
